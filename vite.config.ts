308cabf35fc68503a5c63d4de016204d33ef498a
sabour-agoro <abdoulsabouragoro@gmail.com>
2026-02-15 14:20:52 +0100

    Initialisation et début du projet

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import path from "path" // Si erreur ici -> npm install -D @types/node
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
