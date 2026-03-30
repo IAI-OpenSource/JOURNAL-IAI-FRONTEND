87003a3095c45614dace259a3b171f3d6e737490
benito404 <benitotaylor6@gmail.com>
2026-03-30 22:03:14 +0000

    feat(notifications): la page de notification est terminée

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 import {  Routes, Route } from "react-router-dom";
 import Layout from "./components/squelette/layout";
 import SearchPage from "./pages/SearchPage";
-import UserPage from "./pages/userPage";
+//import UserPage from "./pages/userPage";
+import NotificationPage from "./pages/NotificationPage";
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
         <Route element={<Layout />}>
           {/* La page qu'on veut tester */}
           <Route path="/rechercher" element={<SearchPage />} />
-          <Route path="/userPage" element={<UserPage />} />
+          {/*<Route path="/userPage" element={<UserPage />} />*/}
+          <Route path="/notifications" element={<NotificationPage />} />
         </Route>
       </Routes>
     );
